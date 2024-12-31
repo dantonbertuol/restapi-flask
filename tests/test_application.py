b690f0642649f517ac3201d10ce1584bc3208386
Danton Bertuol <dantonjb@gmail.com>
2024-12-30 22:25:39 -0300

    test: add and get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -8,6 +8,50 @@ class TestApplication:
         app = create_app("config.MockConfig")
         return app.test_client()
 
+    @pytest.fixture
+    def valid_user(self):
+        return {
+            "first_name": "Danton",
+            "last_name": "Bertuol",
+            "cpf": "384.254.930-05",
+            "email": "dantonjb@gmail.com",
+            "birth_date": "1997-03-10",
+        }
+
+    @pytest.fixture
+    def invalid_user(self):
+        return {
+            "first_name": "Danton",
+            "last_name": "Bertuol",
+            "cpf": "384.254.930-06",
+            "email": "dantonjb@gmail.com",
+            "birth_date": "1997-03-10",
+        }
+
     def test_get_users(self, client):
         response = client.get("/users")
         assert response.status_code == 200
+
+    def test_post_user(self, client, valid_user, invalid_user):
+        response = client.post("/user", json=valid_user)
+        assert response.status_code == 200
+        assert b"successfully" in response.data
+
+        response = client.post("/user", json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
+
+    def test_get_user(self, client, valid_user, invalid_user):
+        response = client.get("/user/%s" % valid_user["cpf"])
+        assert response.status_code == 200
+        assert response.json[0]["first_name"] == "Danton"
+        assert response.json[0]["last_name"] == "Bertuol"
+        assert response.json[0]["cpf"] == "384.254.930-05"
+        assert response.json[0]["email"] == "dantonjb@gmail.com"
+
+        birth_date = response.json[0]["birth_date"]["$date"]
+        assert birth_date == "1997-03-10T00:00:00Z"
+
+        response = client.get("/user/%s" % invalid_user["cpf"])
+        assert response.status_code == 400
+        assert b"User does not exist in database!" in response.data
